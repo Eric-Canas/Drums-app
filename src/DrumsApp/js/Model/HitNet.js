2031dc6be5112a4c67cced539aeb48255d83d672
Eric Canas <elcorreodeharu@gmail.com>
2022-09-29 22:32:44 +0200

    Loading the model directly

--- src/DrumsApp/js/Model/HitNet.js
+++ src/DrumsApp/js/Model/HitNet.js
@@ -1,18 +1,28 @@
 
 import {IS_HIT_CONFIDENCE_THRESHOLD, HIT_NET_PATH} from './Constants.js'
 
+const JSON_PATH = "../public/resources/model/model.json";
+const BIN_PATH = "../public/resources/model/group1-shard1of1.bin";
 
 class HitNet{
     constructor(){
         this.jsonFile = document.getElementById("json");
         this.binFile = document.getElementById("bin");
         this.hitNet = null;
-        //this.jsonFile.onchange = () => this._loadModel();
-        this.binFile.onchange = () => this._loadModel();
+        this._loadModel();
     }
     async _loadModel(){
+        const json = await fetch(JSON_PATH);
+        const bin = await fetch(BIN_PATH);
+        // Get blobs
+        const jsonBlob = await json.blob();
+        const binBlob = await bin.blob();
+        console.log(jsonBlob, binBlob);
+        // Create file objects
+        const jsonFile = new File([jsonBlob], "model.json");
+        const binFile = new File([binBlob], "group1-shard1of1.bin");
         this.hitNet = await tf.loadLayersModel(tf.io.browserFiles(
-            [this.jsonFile.files[0], this.binFile.files[0]]));
+            [jsonFile, binFile]));
         console.log("HitNet Charged");
     }
 
